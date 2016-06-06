e7fe95af1cb97809e698488b860928c5be0bb67c
yong8802 <yong880211@naver.com>
2016-06-06 19:44:38 +0900

    Add files via upload

---
+++ gulpfile.babel.js
@@ -0,0 +1,250 @@
+/*! Copyright © 2016 Bapul, jake <gjjoo@bapul.net> */
+
+'use strict';
+
+import path from 'path';
+import del from 'del';
+import runSequence from 'run-sequence';
+import browserSync from 'browser-sync';
+import gulp from 'gulp';
+import {config} from './gulpfile.config';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import buffer from 'vinyl-buffer';
+import source from 'vinyl-source-stream';
+import merge from 'merge-stream';
+import sassdoc from 'sassdoc';
+import pngquant from 'imagemin-pngquant';
+import stylish from 'jshint-stylish';
+import swPrecache from 'sw-precache';
+import pkg from './package.json';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import watchify from 'watchify';
+import assign from 'lodash.assign';
+
+const $ = gulpLoadPlugins();
+
+/**
+ * Default Task
+ * 걸프(Gulp)의 모든 업무 수행
+ */
+gulp.task('default', (cb) => {
+  runSequence(
+    ['sprite'], // svg 아이콘 폰트화 시킬 경우 ,'iconfont' 추가
+    ['views', 'styles', 'scripts', 'images', 'copy'],
+    'generate-service-worker',
+    cb);
+});
+
+/**
+ * Clean Task
+ * 파일 및 폴더 삭제 처리
+ */
+gulp.task('clean', () => {
+  del(config.dir.clean, {dot: true});
+});
+
+/**
+ * Copy Task
+ * 별도의 다른 처리과정 없이 복사만 하여 사용하는 파일들
+ */
+gulp.task('copy', ['copy:font', 'copy:sw-scripts', 'copy:jquery', 'copy:animate', 'copy:lib'], () => {
+  gulp.src(config.dir.copy.root.src)
+    .pipe($.size({title: 'copy: '}))
+    .pipe(gulp.dest(config.dir.copy.root.dest));
+});
+gulp.task('copy:font', () => {
+  gulp.src(config.dir.copy.font.src)
+    .pipe(gulp.dest(config.dir.copy.font.dest));
+});
+gulp.task('copy:jquery', () => {
+  gulp.src(config.dir.copy.jquery.src)
+    .pipe(gulp.dest(config.dir.copy.jquery.dest));
+});
+gulp.task('copy:animate', () => {
+  gulp.src(config.dir.copy.animate.src)
+    .pipe(gulp.dest(config.dir.copy.animate.dest));
+});
+gulp.task('copy:lib', () => {
+  gulp.src(config.dir.copy.lib.src)
+    .pipe(gulp.dest(config.dir.copy.lib.dest));
+});
+gulp.task('copy:sw-scripts', () => {
+  gulp.src(['node_modules/sw-toolbox/sw-toolbox.js', 'src/js/sw/runtime-caching.js'])
+    .pipe(gulp.dest('dist/js/sw'));
+});
+
+/**
+ * Server Task
+ * Javascript, Sass, HTML을 지속적인 감시를 통해 변경 항목만 웹브라우저 자동 리로딩
+ */
+gulp.task('serve', ['views', 'styles', 'scripts'], () => {
+  browserSync.init(config.BROWSER_SYNC);
+
+  gulp.watch([config.dir.html.src], ['views']);
+  gulp.watch([config.dir.sass.src], ['styles']);
+  bundler.on('update', bundleHandler).on('log', $.util.log);
+});
+
+/**
+ * Views Task
+ * HTML 압축
+ */
+gulp.task('views', () => {
+  gulp.src(config.dir.html.src)
+    // .pipe($.htmlmin(config.HTML_MIN)) // HTML 압축
+    .pipe($.size({title: 'views: '}))
+    .pipe(gulp.dest(config.dir.html.dest))
+    .pipe(browserSync.stream());
+});
+
+/**
+ * Styles Task
+ * Sass를 CSS로 변환한다.
+ */
+gulp.task('styles', () => {
+  gulp.src(config.dir.sass.src)
+    .pipe($.sourcemaps.init())
+    // .pipe(sassdoc())
+    .pipe($.sass(config.SASS_OPTION).on('error', $.sass.logError))
+    .pipe($.autoprefixer(config.AUTOPREFIXER))
+    .pipe($.size({title: 'styles: '}))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest(config.dir.sass.dest))
+    .pipe(browserSync.stream());
+});
+
+/**
+ * Styles Lint Task
+ * Sass 유효성 검사
+ */
+gulp.task('styles:lint', () => {
+  gulp.src(config.dir.sass.src)
+    .pipe($.sassLint())
+    .pipe($.sassLint.format())
+    .pipe($.sassLint.failOnError());
+});
+
+/**
+ * Scripts Task
+ * 자바스크립트 번들링
+ */
+const opts = assign({}, watchify.args, config.BROWSERIFY);
+const bundler = watchify(browserify(opts).transform(babelify));
+const bundleHandler = () => {
+  return bundler.bundle()
+    .on('error', $.util.log.bind($.util, 'browserify error'))
+    .pipe(source('script.js'))
+    .pipe(buffer())
+    .pipe($.sourcemaps.init({loadMaps: true}))
+    .pipe($.sourcemaps.write('./'))
+    .pipe($.size({title: 'scripts: '}))
+    .pipe(gulp.dest(config.dir.js.dest))
+    .pipe(browserSync.stream());
+};
+gulp.task('scripts', bundleHandler);
+
+
+
+/**
+ * Scripts Lint Task
+ * 자바스크립트 코드 스타일(jscs) 확인 및 유효성 검사(jshint)
+ */
+gulp.task('scripts:lint', () => {
+  gulp.src(config.dir.js.src)
+    .pipe($.print())
+    .pipe($.jscs())
+    .pipe($.jscs.reporter())
+    .pipe($.jshint())
+    .pipe($.jshint.reporter(stylish));
+});
+
+
+/**
+ * Images Task
+ * 이미지 캐쉬 및 압축
+ */
+gulp.task('images', () => {
+  gulp.src(config.dir.img.src)
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe($.size({title: 'images: '}))
+    .pipe(gulp.dest(config.dir.img.dest));
+});
+
+/**
+ * Sprite Task
+ * 이미지를 스프라이트 처리하여 Request 수 감소
+ */
+gulp.task('sprite', () => {
+  const spriteData = gulp.src('src/images/sprites/*.png').pipe($.spritesmith({
+    imgName: 'sprite.png',
+    imgPath: '../images/sprite.png',
+    cssName: '_sprite.scss',
+    cssTemplate: 'sprites-template.handlebars',
+    padding: 0,
+    algorithm: 'binary-tree'
+  }));
+
+  const imgStream = spriteData.img
+    .pipe(buffer())
+    .pipe($.imagemin({
+      optimizationLevel: 3,
+      use: [pngquant({
+        quality: '65-80',
+        speed: 4
+      })]
+    }))
+    .pipe(gulp.dest('src/images/'));
+
+  const cssStream = spriteData.css
+    .pipe(gulp.dest('src/css/components/'));
+
+  return merge(imgStream, cssStream);
+});
+
+/**
+ * Iconfont Task
+ * SVG를 폰트화 (※ 폰트화 할 경우 단색만 적용 가능)
+ */
+// gulp.task('iconfont', () => {
+//   gulp.src(['src/icons/*.svg'])
+//     .pipe($.iconfontCss({
+//       fontName: 'Icons-Regular',
+//       cssClass: 'bp',
+//       targetPath: '../../styles/components/_icons.scss',
+//       fontPath: '../fonts/Icons/'
+//     }))
+//     .pipe($.iconfont({
+//       fontName: 'Icons-Regular',
+//       prependUnicode: true,
+//       formats: ['ttf', 'eot', 'woff', 'woff2', 'svg']
+//     }))
+//     .pipe(gulp.dest('src/fonts/Icons/'));
+// });
+
+/**
+ * Service Worker Task
+ * 파일을 캐쉬하여 사용하고자 할 경우 (※ Production에서만 사용)
+ */
+gulp.task('generate-service-worker', ['copy:sw-scripts'], () => {
+  const rootDir = 'dist';
+  const filepath = path.join(rootDir, 'service-worker.js');
+
+  return swPrecache.write(filepath, {
+    cacheId: pkg.name || 'starter-kit',
+    importScripts: [
+      'js/sw/sw-toolbox.js',
+      'js/sw/runtime-caching.js'
+    ],
+    staticFileGlobs: [
+      `${rootDir}/images/**/*`,
+      `${rootDir}/js/**/*.js`,
+      `${rootDir}/css/**/*.css`,
+      `${rootDir}/*.{html,json}`
+    ],
+    stripPrefix: rootDir + '/'
+  });
+});
---
+++ gulpfile.config.js
@@ -0,0 +1,180 @@
+
+'use strict';
+
+
+/**
+ * 개발 소스 폴더 경로
+ * @type {string}
+ * @private
+ */
+const _bower = 'bower_components/';
+
+
+/**
+ * 개발 소스 폴더 경로
+ * @type {string}
+ * @private
+ */
+const _source = 'src/';
+
+/**
+ * 최종 소스 폴더 경로
+ * @type {string}
+ * @private
+ */
+const _development = 'dist/';
+
+/**
+ * 폴더 경로 설정
+ * @type {object}
+ * @private
+ */
+const _dir = {
+  // 제거
+  clean: ['dist/*'],
+
+  // 복사
+  copy: {
+    root: {
+      src: [
+        _source + 'favicon.ico',
+        // _source + 'humans.txt',
+        // _source + 'manifest.json',
+        // _source + 'manifest.webapp',
+        // _source + 'robots.txt'
+      ],
+      dest: _development
+    },
+    font: {
+      src: _source + 'fonts/**/*',
+      dest: _development + 'fonts'
+    },
+    jquery: {
+      src: _bower + 'jquery/dist/jquery.js',
+      dest: _development + 'js/'
+    },
+    animate: {
+      src: _bower + 'animate.css/animate.css',
+      dest: _development + 'css/'
+    },
+    lib : {
+      src: _source + 'js/lib/**/*.js',
+      dest: _development + 'js/'
+    }
+  },
+
+  // HTML
+  html: {
+    src: _source + '**/*.html',
+    dest: _development
+  },
+
+  // SASS
+  sass: {
+    src: _source + 'sass/**/*.{scss,sass}',
+    dest: _development + 'css/'
+  },
+
+  // JS
+  js: {
+    src: [
+      _source + 'js/**/*.js'
+    ],
+    dest: _development + 'js/'
+  },
+
+  // IMG
+  img: {
+    src: ['src/images/**/*'],
+    dest: _development + 'images/'
+  }
+};
+
+/**
+ * browser-sync options
+ * @reference https://www.browsersync.io/docs/gulp/
+ * @private
+ */
+const _BROWSER_SYNC = {
+  server: {
+    baseDir: _development
+  },
+  port: 3000,   // 포트
+  open: true    // 웹서버 실행 시 자동 파일 실행 여부
+};
+
+/**
+ * gulp-sass options
+ * @reference https://github.com/sass/node-sass#options
+ * @private
+ */
+const _SASS_OPTION = {
+  outputStyle: 'expanded',  // CSS 출력 스타일 (expanded, nested, compact, compressed)
+  indentType: 'space',        // 들여쓰기 유형 (space, tab)
+  indentWidth: 2,             // 들여쓰기 폭 (2 ~ 10)
+  percision: 4,               // 수치 정확도 (소수점 이하 자리 수)
+  sourceMap: false            // 소스맵 작성 설정
+};
+
+/**
+ * gulp-autoprefixer options
+ * @reference https://github.com/postcss/autoprefixer#options
+ * @private
+ */
+const _AUTOPREFIXER = [
+  'ie >= 10',       // Internet Explorer
+  'ie_mob >= 10',   // Internet Explorer Mobile
+  'ff >= 30',       // Mozilla Firefox
+  'chrome >= 34',   // Google Chorme
+  'safari >= 7',    // Desktop Safari
+  'opera >= 23',    // Opera
+  'ios >= 7',       // iOS Safari
+  'android >= 4.4', // Android WebView
+  'bb >= 10'        // Blackberry browser
+];
+
+/**
+ * gulp-htmlmin options
+ * @reference https://github.com/kangax/html-minifier
+ * @private
+ */
+const _HTML_MIN = {
+  collapseWhitespace: true
+};
+
+/**
+ * gulp-uglify options
+ * @reference https://github.com/terinjokes/gulp-uglify#options
+ * @private
+ */
+const _UGLIFY = {
+  preserveComments: 'some'
+};
+
+/**
+ * browserify options
+ * @reference https://www.npmjs.com/package/browserify#browserifyfiles--opts
+ * @private
+ */
+const _BROWSERIFY = {
+  entries: './src/js/script.js', // 번들 엔트리 파일 설정
+  debug: true,                        // 소스맵 파일에 포함 설정
+  extensions: ['js'],                 // 파일 확장자 설정
+  insertGlobals: true                 // 글로벌 삽입 (process, global, __filename, __Dirname) 항상 삽입, ※ 빌드 속도는 향상하지만 출력(Output) 파일 크기는 커짐
+};
+
+/**
+ * Export Gulp Config
+ */
+export const config = {
+  src: _source,
+  dev: _development,
+  dir: _dir,
+
+  BROWSER_SYNC: _BROWSER_SYNC,
+  SASS_OPTION: _SASS_OPTION,
+  AUTOPREFIXER: _AUTOPREFIXER,
+  HTML_MIN: _HTML_MIN,
+  UGLIFY: _UGLIFY,
+  BROWSERIFY: _BROWSERIFY
+};
